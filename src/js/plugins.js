4f68236c8769341897359df4a21c9b07fcb6d84f
maunilswadas <swadasmaunil@gmail.com>
2016-03-27 19:46:45 +0530

    Add music player added,scripting of music source id changed, new svg added for music player

--- src/js/plugins.js
+++ src/js/plugins.js
@@ -22,3 +22,6 @@
 }());
 
 // Place any jQuery/helper plugins in here.
+$('.track').click(function(){
+    change_track($(this).attr('data-location'));// function to change the track of the loaded audio player without page refresh preferred...
+});
